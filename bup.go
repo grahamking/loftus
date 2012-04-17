cb309e319eae1f0e5ed32062765551c219ef0e23
Graham King <graham@gkgk.org>
2012-04-16 22:55:23 -0700

    go fmt. Shows git errors by calling script bup_alert, which is expected to be in path - make a shell script to wrap your favorite zenity.

--- bup.go
+++ bup.go
@@ -1,196 +1,206 @@
 package main
 
 import (
-    "os"
-    "log"
-    "strings"
-    "flag"
-    "exp/inotify"
-    "path/filepath"
+	"exp/inotify"
+	"flag"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 const (
-    INTERESTING = inotify.IN_MODIFY | inotify.IN_CREATE | inotify.IN_DELETE | inotify.IN_MOVE
+	INTERESTING = inotify.IN_MODIFY | inotify.IN_CREATE | inotify.IN_DELETE | inotify.IN_MOVE
 )
 
 type Backend interface {
-    Sync()
-    Changed(filename string)
-    ShouldWatch(filename string) bool
-    RegisterPushHook(func())
+	Sync() error
+	Changed(filename string)
+	ShouldWatch(filename string) bool
+	RegisterPushHook(func())
 }
 
 type Config struct {
-    isServer bool
-    isCheck bool
-    serverAddr string
-    syncDir string
-    logDir string
-    stdout bool
+	isServer   bool
+	isCheck    bool
+	serverAddr string
+	syncDir    string
+	logDir     string
+	stdout     bool
 }
 
 type Client struct {
-    backend Backend
-    rootDir string
-    watcher *inotify.Watcher
-    logger *log.Logger
-    incoming chan string
+	backend  Backend
+	rootDir  string
+	watcher  *inotify.Watcher
+	logger   *log.Logger
+	incoming chan string
 }
 
 func main() {
 
-    config := confFromFlags()
-    logDir := config.logDir
-    log.Println("Logging to ", logDir)
+	config := confFromFlags()
+	logDir := config.logDir
+	log.Println("Logging to ", logDir)
 
-    os.Mkdir(config.logDir, 0750)
-    os.Mkdir(config.syncDir, 0750)
+	os.Mkdir(config.logDir, 0750)
+	os.Mkdir(config.syncDir, 0750)
 
-    if config.isCheck {
-        runCheck(config)
+	if config.isCheck {
+		runCheck(config)
 
-    } else if config.isServer {
-        startServer(config)
+	} else if config.isServer {
+		startServer(config)
 
-    } else {
-        startClient(config)
-    }
+	} else {
+		startClient(config)
+	}
 }
 
 // Parse commands line flags in to a configuration object
 func confFromFlags() *Config {
 
-    defaultSync := os.Getenv("HOME") + "/bup/"
-    var syncDir = flag.String(
-        "dir",
-        defaultSync,
-        "Synchronise this directory. Must already be a git repo with a remote (i.e. 'git pull' works)")
+	defaultSync := os.Getenv("HOME") + "/bup/"
+	var syncDir = flag.String(
+		"dir",
+		defaultSync,
+		"Synchronise this directory. Must already be a git repo with a remote (i.e. 'git pull' works)")
 
-    var isServer = flag.Bool("server", false, "Be the server")
-    var serverAddr = flag.String(
-        "address",
-        "127.0.0.1:8007",
-        "address:post where server is listening")
+	var isServer = flag.Bool("server", false, "Be the server")
+	var serverAddr = flag.String(
+		"address",
+		"127.0.0.1:8007",
+		"address:post where server is listening")
 
-    var isCheck = flag.Bool("check", false, "Check we are setup correctly")
+	var isCheck = flag.Bool("check", false, "Check we are setup correctly")
 
-    defaultLog := os.Getenv("HOME") + "/.bup/"
-    var logDir = flag.String("log", defaultLog, "Log directory")
+	defaultLog := os.Getenv("HOME") + "/.bup/"
+	var logDir = flag.String("log", defaultLog, "Log directory")
 
-    var stdout = flag.Bool("stdout", false, "Log to stdout")
+	var stdout = flag.Bool("stdout", false, "Log to stdout")
 
-    flag.Parse()
+	flag.Parse()
 
-    return &Config{
-        isServer: *isServer,
-        isCheck: *isCheck,
-        serverAddr: *serverAddr,
-        syncDir: *syncDir,
-        logDir: *logDir,
-        stdout: *stdout}
+	return &Config{
+		isServer:   *isServer,
+		isCheck:    *isCheck,
+		serverAddr: *serverAddr,
+		syncDir:    *syncDir,
+		logDir:     *logDir,
+		stdout:     *stdout}
 }
 
 // Watch directories, called sync methods on backend, etc
 func startClient(config *Config) {
 
-    syncDir := config.syncDir
+	syncDir := config.syncDir
 
-    logger := openLog(config, "client.log")
+	logger := openLog(config, "client.log")
 
-    logger.Println("Synchronising: ", syncDir)
+	logger.Println("Synchronising: ", syncDir)
 
-    syncDir = strings.TrimRight(syncDir, "/")
-    backend := NewGitBackend(config)
+	syncDir = strings.TrimRight(syncDir, "/")
+	backend := NewGitBackend(config)
 
-    watcher, _ := inotify.NewWatcher()
+	watcher, _ := inotify.NewWatcher()
 
-    incomingChannel := make(chan string)
+	incomingChannel := make(chan string)
 
-    client := Client{
-        rootDir: syncDir,
-        backend: backend,
-        watcher: watcher,
-        logger: logger,
-        incoming: incomingChannel,
-    }
-    client.addWatches()
+	client := Client{
+		rootDir:  syncDir,
+		backend:  backend,
+		watcher:  watcher,
+		logger:   logger,
+		incoming: incomingChannel,
+	}
+	client.addWatches()
 
-    // Always start with a sync to bring us up to date
-    backend.Sync()
+	// Always start with a sync to bring us up to date
+	err := backend.Sync()
+	if err != nil {
+		Warn(err.Error())
+	}
 
-    go udpListen(logger, incomingChannel)
-    go tcpListen(logger, config.serverAddr, incomingChannel)
-    client.run()
+	go udpListen(logger, incomingChannel)
+	go tcpListen(logger, config.serverAddr, incomingChannel)
+	client.run()
 }
 
 func openLog(config *Config, name string) *log.Logger {
 
-    if config.stdout {
-        return log.New(os.Stdout, "", log.LstdFlags)
-    }
+	if config.stdout {
+		return log.New(os.Stdout, "", log.LstdFlags)
+	}
 
-    writer, err := os.OpenFile(
-        config.logDir + name, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0650)
+	writer, err := os.OpenFile(
+		config.logDir+name, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0650)
 
-    if err != nil {
-        log.Fatal("Error opening log file", name, " in ", config.logDir, err)
-    }
+	if err != nil {
+		log.Fatal("Error opening log file", name, " in ", config.logDir, err)
+	}
 
-    return log.New(writer, "", log.LstdFlags)
+	return log.New(writer, "", log.LstdFlags)
 }
 
 // Main loop
 func (self *Client) run() {
 
-    // push hook will be called from a go routine
-    self.backend.RegisterPushHook(func() {
-        msg := "Updated\n"
-        if remoteConn != nil { // remoteConn is global in comms.go
-            tcpSend(remoteConn, msg)
-        }
-        udpSend(self.logger, msg)
-    })
+	// push hook will be called from a go routine
+	self.backend.RegisterPushHook(func() {
+		msg := "Updated\n"
+		if remoteConn != nil { // remoteConn is global in comms.go
+			tcpSend(remoteConn, msg)
+		}
+		udpSend(self.logger, msg)
+	})
 
-    for {
-        select {
-        case ev := <-self.watcher.Event:
+	for {
+		select {
+		case ev := <-self.watcher.Event:
 
-            self.logger.Println(ev)
+			self.logger.Println(ev)
 
-            isCreate := ev.Mask & inotify.IN_CREATE != 0
-            isDir := ev.Mask & inotify.IN_ISDIR != 0
+			isCreate := ev.Mask&inotify.IN_CREATE != 0
+			isDir := ev.Mask&inotify.IN_ISDIR != 0
 
-            if isCreate && isDir && self.backend.ShouldWatch(ev.Name) {
-                self.logger.Println("Adding watch", ev.Name)
-                self.watcher.AddWatch(ev.Name, INTERESTING)
-            }
+			if isCreate && isDir && self.backend.ShouldWatch(ev.Name) {
+				self.logger.Println("Adding watch", ev.Name)
+				self.watcher.AddWatch(ev.Name, INTERESTING)
+			}
 
-            self.backend.Changed(ev.Name)
+			self.backend.Changed(ev.Name)
 
-        case err := <-self.watcher.Error:
-            self.logger.Println("error:", err)
+		case err := <-self.watcher.Error:
+			self.logger.Println("error:", err)
 
-        case <-self.incoming:
-            self.logger.Println("Remote update notification")
-            self.backend.Sync()
-        }
+		case <-self.incoming:
+			self.logger.Println("Remote update notification")
+			self.backend.Sync()
+		}
 
-    }
+	}
 }
 
 // Add inotify watches on rootDir and all sub-dirs
 func (self *Client) addWatches() {
 
-    addSingleWatch := func(path string, info os.FileInfo, err error) error {
-        if info.IsDir() && self.backend.ShouldWatch(path) {
-            self.logger.Println("Watching", path)
-            self.watcher.AddWatch(path, INTERESTING)
-        }
-        return nil
-    }
+	addSingleWatch := func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() && self.backend.ShouldWatch(path) {
+			self.logger.Println("Watching", path)
+			self.watcher.AddWatch(path, INTERESTING)
+		}
+		return nil
+	}
 
-    err := filepath.Walk(self.rootDir, addSingleWatch)
-    if err != nil {
-        self.logger.Fatal(err)
-    }
+	err := filepath.Walk(self.rootDir, addSingleWatch)
+	if err != nil {
+		self.logger.Fatal(err)
+	}
+}
+
+// Utility function to warn user about something - for example a git error
+func Warn(msg string) {
+	cmd := exec.Command("bup_alert", msg)
+	cmd.Run()
 }
--- git-backend.go
+++ git-backend.go
@@ -1,140 +1,156 @@
 package main
 
 import (
-    "log"
-    "strings"
-    "os/exec"
-    "time"
-    "sync"
+	"log"
+	"os/exec"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
 )
 
 const (
-    SYNC_PAUSE = 2
-    PUSH_PAUSE = 10
+	SYNC_PAUSE = 2
+	PUSH_PAUSE = 10
 )
 
 type GitBackend struct {
-    logger *log.Logger
-    gitPath string
+	logger  *log.Logger
+	gitPath string
 
-    rootDir string
+	rootDir string
 
-    syncLock sync.Mutex
-    isSyncPending bool
+	syncLock      sync.Mutex
+	isSyncPending bool
 
-    pushLock sync.Mutex
-    isPushPending bool
+	pushLock      sync.Mutex
+	isPushPending bool
 
-    pushHook func()
+	pushHook func()
 
-    isPullActive bool   // Is a pull currently running - ignore all events
+	isPullActive bool // Is a pull currently running - ignore all events
 }
 
 func NewGitBackend(config *Config) *GitBackend {
 
-    rootDir := config.syncDir
-    logger := openLog(config, "git.log")
+	rootDir := config.syncDir
+	logger := openLog(config, "git.log")
 
-    gitPath, err := exec.LookPath("git")
-    if err != nil {
-        logger.Fatal("Error looking for 'git' on path. ", err)
-    }
+	gitPath, err := exec.LookPath("git")
+	if err != nil {
+		logger.Fatal("Error looking for 'git' on path. ", err)
+	}
 
-    return &GitBackend{
-        logger: logger,
-        rootDir: rootDir,
-        gitPath: gitPath,
-    }
+	return &GitBackend{
+		logger:  logger,
+		rootDir: rootDir,
+		gitPath: gitPath,
+	}
 }
 
 // A file or directory has been created
 func (self *GitBackend) Changed(filename string) {
-    if self.isGit(filename) || self.isPullActive {
-        return
-    }
-    go self.syncLater()
+	if self.isGit(filename) || self.isPullActive {
+		return
+	}
+	go self.syncLater()
 }
 
 // Run: git add --all ; git commit --all
-func (self *GitBackend) Sync() {
+func (self *GitBackend) Sync() error {
 
-    // Pull first to ensure a fast-forward when we push
-    self.pull()
+	var err error
 
-    self.git("add", "--all")
-    err := self.git("commit", "--all", "--message=bup")
+	// Pull first to ensure a fast-forward when we push
+	err = self.pull()
+	if err != nil {
+		return err
+	}
 
-    if err == nil {
-        go self.pushLater()
-    }
+	err = self.git("add", "--all")
+	if err != nil {
+		return err
+	}
+
+	err = self.git("commit", "--all", "--message=bup")
+	if err != nil {
+		return err
+	}
+
+	go self.pushLater()
+
+	return nil
 }
 
 // Register the function to be called after we push to remote
 func (self *GitBackend) RegisterPushHook(callback func()) {
-    self.pushHook = callback
+	self.pushHook = callback
 }
 
 // Should the inotify watch watch the given path
 func (self *GitBackend) ShouldWatch(filename string) bool {
-    return !self.isGit(filename)
+	return !self.isGit(filename)
 }
 
 // Is filename inside a .git directory?
 func (self *GitBackend) isGit(filename string) bool {
-    return strings.Contains(filename, ".git")
+	return strings.Contains(filename, ".git")
 }
 
 // Schedule a synchronise for in a few seconds
 func (self *GitBackend) syncLater() {
 
-    // ensure only once per time - might be able to use sync.Once instead (?)
-    self.syncLock.Lock()
-    if self.isSyncPending {
-        self.syncLock.Unlock()
-        return
-    }
-    self.isSyncPending = true
-    self.syncLock.Unlock()
+	// ensure only once per time - might be able to use sync.Once instead (?)
+	self.syncLock.Lock()
+	if self.isSyncPending {
+		self.syncLock.Unlock()
+		return
+	}
+	self.isSyncPending = true
+	self.syncLock.Unlock()
 
-    time.Sleep(SYNC_PAUSE * time.Second)
-    self.Sync()
+	time.Sleep(SYNC_PAUSE * time.Second)
+	self.Sync()
 
-    self.isSyncPending = false
+	self.isSyncPending = false
 }
 
 // Schedule a push for later
 func (self *GitBackend) pushLater() {
 
-    // ensure only once per time - might be able to use sync.Once instead (?)
-    self.pushLock.Lock()
-    if self.isPushPending {
-        self.pushLock.Unlock()
-        return
-    }
-    self.isPushPending = true
-    self.pushLock.Unlock()
+	// ensure only once per time - might be able to use sync.Once instead (?)
+	self.pushLock.Lock()
+	if self.isPushPending {
+		self.pushLock.Unlock()
+		return
+	}
+	self.isPushPending = true
+	self.pushLock.Unlock()
 
-    time.Sleep(PUSH_PAUSE * time.Second)
-    self.push()
+	time.Sleep(PUSH_PAUSE * time.Second)
+	err := self.push()
+	if err != nil {
+		Warn(err.Error())
+	}
 
-    self.isPushPending = false
+	self.isPushPending = false
 }
 
 // Run: git push
 func (self *GitBackend) push() error {
-    err := self.git("push")
-    if err == nil && self.pushHook != nil {
-        go self.pushHook()
-    }
-    return err
+	err := self.git("push")
+	if err == nil && self.pushHook != nil {
+		go self.pushHook()
+	}
+	return err
 }
 
 // Run: git pull
 func (self *GitBackend) pull() error {
-    self.isPullActive = true
-    err := self.git("pull")
-    self.isPullActive = false
-    return err
+	self.isPullActive = true
+	err := self.git("pull")
+	self.isPullActive = false
+	return err
 }
 
 /* Runs a git command, returns nil if success, error if err
@@ -143,17 +159,39 @@ func (self *GitBackend) pull() error {
 */
 func (self *GitBackend) git(gitCmd string, args ...string) error {
 
-    cmd := exec.Command(self.gitPath, append([]string{gitCmd}, args...)...)
-    cmd.Dir = self.rootDir
-    self.logger.Println(cmd)
+	cmd := exec.Command(self.gitPath, append([]string{gitCmd}, args...)...)
+	cmd.Dir = self.rootDir
+	self.logger.Println(cmd)
 
-    output, err := cmd.CombinedOutput()
-    self.logger.Println(string(output))
+	output, err := cmd.CombinedOutput()
+	self.logger.Println(string(output))
 
-    if err != nil {
-        self.logger.Println(err)
-        return err
-    }
+	if err != nil {
 
-    return nil
+		exitStatus := err.(*exec.ExitError).Sys().(syscall.WaitStatus).ExitStatus()
+		if exitStatus != 1 {
+			self.logger.Println(err)
+			gitErr := &GitError{
+                cmd: strings.Join(cmd.Args, " "),
+                internalError: err,
+                output: string(output)}
+			return gitErr
+		}
+	}
+
+	return nil
+}
+
+type GitError struct {
+	cmd           string
+	internalError error
+	output        string
+}
+
+// error implementation which displays git info
+func (self *GitError) Error() string {
+    msg := "git error running: " + self.cmd + "\n\n"
+    msg += self.output + "\n"
+	msg += self.internalError.Error()
+    return msg
 }
